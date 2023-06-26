a77951d0b718fc8319ef0764693de2952fabb39f
Vu The Song <songvt.bhsoft@gmail.com>
2023-06-26 17:12:30 +0700

    update font-size

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var btnMenu = document.getElementById("btn");
 var nav = document.getElementById("nav");
 var btn = document.getElementsByClassName("btn-nav");
+
+var btnScroll = document.getElementById("btn-scroll-top");
 btnMenu.onclick = function () {
   if (nav.classList.contains("display-none")) {
     nav.classList.remove("display-none");
@@ -14,3 +16,24 @@ for (var i = 0; i < btn.length; i++) {
     nav.classList.add("display-none");
   };
 }
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 3500 ||
+    document.documentElement.scrollTop > 3500
+  ) {
+    btnScroll.style.display = "block";
+  } else {
+    btnScroll.style.display = "none";
+  }
+}
+
+btnScroll.onclick = function () {
+  console.log(123);
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
